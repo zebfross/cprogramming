e98ea3d3c3a7e0430d3599c7c0ed5e4e525bc3d0
= <zebfross@hotmail.com>
2015-01-23 23:00:06 -0700

    added variables to calculator

--- chapter3/calculator.c
+++ chapter3/calculator.c
@@ -7,17 +7,31 @@
 #define MAXOP 100
 #define NUMBER '0'
 #define FUNCTION 'a'
+#define VARIABLE 'v'
+#define NUM_VARS 'z' - 'A' + 1
 
 int getop(char []);
 void push(double);
 double pop(void);
 double peek(void);
+void dup(void);
+void swap(void);
+void clear(void);
+int isVarDefined(char);
+void defineVar(char, double);
+double getVar(char);
+short varDefs[NUM_VARS];
+double vars[NUM_VARS];
 
 /* negative numbers are represented by an underscore '_' instead of a dash */
 main() {
 	int type;
 	double op2;
 	char s[MAXOP];
+
+	int i;
+	for(i = 0; i < NUM_VARS; ++i)
+		varDefs[i] = 0;
 
 	while ((type = getop(s)) != EOF) {
 		switch (type) {
@@ -31,6 +45,21 @@ main() {
 				push(cos(pop()));
 			else if(strcmp(s, "tan") == 0)
 				push(tan(pop()));
+			else if(strcmp(s, "exp") == 0)
+				push(exp(pop()));
+			else if(strcmp(s, "swap") == 0)
+				swap();
+			else if(strcmp(s, "dup") == 0)
+				dup();
+			else if(strcmp(s, "clear") == 0)
+				clear();
+			else if(s[0] == '=')
+				defineVar(s[1], peek());
+			else if(strlen(s) == 1) /* is variable */
+				if(isVarDefined(s[0]))
+					push(getVar(s[0]));
+				else
+					printf("error: variable %c is undefined\n", s[0]);
 			else
 				printf("unknown function reference %s\n", s);
 			break;
@@ -58,6 +87,10 @@ main() {
 			else
 				printf("error: zero division\n");
 			break;
+		case '^':
+			op2 = pop();
+			push(pow(pop(), op2));
+			break;
 		case '\n':
 			printf("\t%.8g\n", peek());
 			break;
@@ -81,7 +114,7 @@ void push(double f) {
 		printf("error: stack full, can't push %g\n", f);
 }
 
-double pop(void) {
+double pop() {
 	if(sp > 0)
 		return val[--sp];
 	else
@@ -89,7 +122,7 @@ double pop(void) {
 	return 0.0;
 }
 
-double peek(void) {
+double peek() {
 	if(sp > 0)
 		return val[sp-1];
 	else
@@ -97,14 +130,24 @@ double peek(void) {
 	return 0.0;
 }
 
-void swap(void) {
+void dup() {
+	val[sp] = val[sp - 1];
+	++sp;
+}
+
+void swap() {
 	if(sp > 1) {
 		double temp = val[sp-2];
 		val[sp-2] = val[sp-1];
 		val[sp-1] = temp;
 	} else {
-		printf("error; stack empty\n");
+		printf("error; less than 2 elements in stack\n");
 	}
+}
+
+void clear(void) {
+	sp = 0;
+	printf("stack cleared\n");
 }
 
 int getch(void);
@@ -117,7 +160,7 @@ int getop(char s[]) {
 	while ((s[0] = c = getch()) == ' ' || c == '\t')
 		;
 	s[1] = '\0';
-	if(isalpha(c)) {
+	if(isalpha(c) || c == '=') {
 		while(isalpha(s[++i] = c = getch()))
 			;
 		ret = FUNCTION;
@@ -139,6 +182,19 @@ int getop(char s[]) {
 	if(c != EOF)
 		ungetch(c);
 	return ret;
+}
+
+int isVarDefined(char v) {
+	return varDefs['z' - v];
+}
+
+void defineVar(char v, double val) {
+	varDefs['z' - v] = 1;
+	vars['z' - v] = val;
+}
+
+double getVar(char v) {
+	return vars['z' - v];
 }
 
 #define BUFSIZE 100
